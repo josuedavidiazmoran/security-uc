580ab60e22b0c6e0abdebf066ae1c4da248dada2
DReyna <reyna_b@upeu.edu.pe>
2021-11-12 15:57:21 -0500

    seguridad con token

---
+++ src/main/java/uc/us_security/UsSecurityApplication.java
@@ -0,0 +1,13 @@
+package uc.us_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uc/us_security/UsSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package uc.us_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
