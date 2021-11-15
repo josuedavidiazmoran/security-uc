d1e4dba84019d778dafac307168c33c40d887020
J05K4 <73666329+josuedavidiazmoran@users.noreply.github.com>
2021-11-15 16:41:38 -0500

    adfa
    asdfasdf

--- src/main/java/uc/us_security/UsSecurityApplication.java
+++ src/main/java/uc/us_security/UsSecurityApplication.java
@@ -2,12 +2,18 @@ package uc.us_security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class UsSecurityApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UsSecurityApplication.class, args);
-	}
+    static BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder(); 
+    
+    public static void main(String[] args) {
+       
+        SpringApplication.run(UsSecurityApplication.class, args);
+
+        System.out.println(bCryptPasswordEncoder.encode("123").toString()); 
+    }
 
 }
